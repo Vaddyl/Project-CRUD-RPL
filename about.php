24839581a9d727a4a17c52af96b9d30e1b82c1b4
Vaddyl <29392110+Vaddyl@users.noreply.github.com>
2018-03-24 12:25:53 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<?php 
+include "header.php";
+include "alerts.php";
+include "connect.php"; 
+?>
+</head>
+
+<body>
+<div class="container">
+<?php include "nav.php"; ?>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="page-header">
+            <h1>About</h1>
+        </div>
+    </div>
+</div>
+
+
+<p> Name : Fadilah Agung Nugraha <br />
+	NIM	 : G64160045 <br />
+	 <a target="_blank" href="https://github.com/Vaddyl/">My Github</a>  <br />
+	 <a target="_blank" href="https://github.com/Vaddyl/Project-CRUD-RPL/">Link to the project</a>  <br />
+This is my work for individual Software Engineering CRUD(Create,Read,Update,Delele) project, Create function is on index or main site, rest on list.php. Im sorry for being that weebs. Thank you.</p>
+
+<br />
+
+
+</div>
+<?php include "footer.php"; ?>
+</body>
+</html>
---
+++ abouts.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<?php 
+include "header.php";
+include "alerts.php";
+include "connect.php"; 
+?>
+</head>
+
+<body>
+<div class="container">
+<?php include "nav.php"; ?>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="page-header">
+            <h1>About Satania</h1>
+        </div>
+    </div>
+</div>
+
+
+<p> She is one of the main character in anime series
+	 <a target="_blank" href="https://myanimelist.net/anime/33731/Gabriel_DropOut">Gabriel DropOut</a>. She's the best. 
+</p>
+    <img src="satania_cute.png" width="1150" alt="cute">
+<br />
+
+
+</div>
+<?php include "footer.php"; ?>
+</body>
+</html>
